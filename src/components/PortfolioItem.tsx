2d6e83b23b32159fe48f76cbbc3bcabf19b02e63
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-07-13 13:08:19 -0600

    Update PortfolioItem.tsx

--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -7,42 +7,42 @@ import {
   Image,
   Icon,
   Text,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
-import { PortfolioItemProps } from "../interfaces/PortfolioItemProps";
-import { Link } from "react-router-dom";
-import { Icons } from "../assets/misc/Icons";
+import { PortfolioItemProps } from '../interfaces/PortfolioItemProps';
+import { Link } from 'react-router-dom';
+import { Icons } from '../assets/misc/Icons';
 
 export default function PortfolioItem({
   portfolio,
 }: {
   portfolio: PortfolioItemProps;
 }) {
-  const { url, imageUrl, title, description, icons } = portfolio;
+  const { imageUrl, title, description, icons } = portfolio;
 
   return (
-    <Link to={url}>
+    <Link to={'#'}>
       <Box
-        w={"full"}
-        backgroundColor={useColorModeValue("#fcfcfd", "#211E2B")}
-        rounded={"md"}
+        w={'full'}
+        backgroundColor={useColorModeValue('#fcfcfd', '#211E2B')}
+        rounded={'md'}
         p={6}
-        overflow={"hidden"}
+        overflow={'hidden'}
       >
-        <Box bg={"gray.100"} mt={-6} mx={-6} mb={6} pos={"relative"}>
+        <Box bg={'gray.100'} mt={-6} mx={-6} mb={6} pos={'relative'}>
           <Image
             src={imageUrl}
-            fit={"fill"}
-            minW={"100%"}
-            fallbackSrc={"/img/spinner.gif"}
+            fit={'fill'}
+            minW={'100%'}
+            fallbackSrc={'/img/spinner.gif'}
           />
         </Box>
-        <Stack minH={"4rem"}>
+        <Stack minH={'4rem'}>
           <Box>
             <Heading
-              color={useColorModeValue("gray.700", "white")}
-              fontSize={"2xl"}
-              fontFamily={"body"}
+              color={useColorModeValue('gray.700', 'white')}
+              fontSize={'2xl'}
+              fontFamily={'body'}
             >
               {title}
             </Heading>
@@ -55,8 +55,9 @@ export default function PortfolioItem({
               return (
                 <Icon
                   key={index}
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
                   as={Icons[icon as keyof typeof Icons]}
-                  alt={"Ionic"}
+                  alt={'Ionic'}
                   mx={2}
                   boxSize={6}
                 />
