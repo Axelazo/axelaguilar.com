749167ec5559296becbe2fd07a53648b15f0ac26
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-07-13 11:30:59 -0600

    Delete EducationItemList.jsx

--- src/components/EducationItemList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import EducationItem from '../components/EducationItem';
-
-export default function EducationItemList(props) {
-  return props.educations.map((education, index) => {
-    return <EducationItem key={index} education={education}></EducationItem>;
-  });
-}
